aa9ddeebc9b70da22bfdaf2d68657fc6757716f2
rhexa <akun.kripitit@gmail.com>
2021-05-26 22:06:43 +0300

    added tester file

---
+++ testFs.js
@@ -0,0 +1,21 @@
+const { ifError } = require('assert');
+const fs = require('fs');
+let fileWritten = false;
+
+fs.writeFile('./out.txt', "test from here", (err, data)=>{
+    if (err) return console.log(err);
+    console.log("hello from unexecuted callback");
+    fileWritten = true;
+});
+
+
+console.log("first log");
+
+const cbTimer = setInterval(() => {
+    if (fileWritten === true) {
+        console.log(fileWritten)
+        clearInterval(cbTimer)
+    };
+}, 100);
+
+console.log("after the call back");
