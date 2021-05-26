6fe5a8efb9b1d3892064c9b846d53ad47ffece80
rhexa <akun.kripitit@gmail.com>
2021-05-26 12:50:56 +0300

    fixed model and controller

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const router = require("../course/Router");
+const router = require('./Router');
 var path = require('path');
 const app = express();
-const movies = require('../course/models/movie');
-let Movie = movies.Movie;
 
 const port = 3000;
 const base = 'http://localhost';
@@ -13,12 +11,4 @@ app.set('base', `${base}:${port}`);
 app.use('/api', router);
 app.use('/public/uploads',express.static(path.join(__dirname, 'public/uploads')));
 
-let movie1 = new Movie();
-movie1.id = '12345';
-movie1.title = 'Doraemon';
-
-movies.addMovie(movie1);
-console.table(movies.getMovies());
-
-
-app.listen(port, ()=>console.log(`Server has been started and listening on port ${port}`));
+app.listen(port, ()=>console.log(`Express Crud server has been started and listening on port ${port}`));
--- models/movie.js
+++ models/movie.js
@@ -1,72 +1,40 @@
 const movies = [
-    {id: '1588323375416', title: 'Star Wars: Episode IX - The Rise of Skywalker', year: 2019, director: 'J.J. Abrams'},
-    {id: '1588323390624', title: 'The Irishman', year: 2019, director: 'Martin Scorsese'},
-    {id: '1588323412643', title: 'Harry Potter and the Sorcerers Stone', year: 2001, director: 'Chris Columbus'}
+    {id: '1588323375416', title: 'Star Wars: Episode IX - The Rise of Skywalker', picture:{}, year: 2019, director: 'J.J. Abrams'},
+    {id: '1588323390624', title: 'The Irishman', picture:{}, year: 2019, director: 'Martin Scorsese'},
+    {id: '1588323412643', title: 'Harry Potter and the Sorcerers Stone', picture:{}, year: 2001, director: 'Chris Columbus'}
   ];
 
-// let Movie = {
-//     id: '',
-//     title: '',
-//     year: 2020,
-//     director: 'Default'
-// }
-
-// function Movie (id, title, year=2020, director='Default') {
-//     this.id = id,
-//     this.title = title,
-//     this.year = year,
-//     this.director = director
-
-//     let getId = ()=> {
-//         return this.id;
-//     };
-//     let setId = (id)=> {
-//         this.id = id;
-//     };
-//     let getTitle = () => {
-//         return this.title;
-//     };
-//     let setTitle = (title) => {
-//         this.title = title;
-//     };
-// }
-
-function Movie (id, title, year=2020, director='Default') {
-    
-    let getId = ()=> {
-        return this.id;
-    };
-    let setId = (id)=> {
-        this.id = id;
-    };
-    let getTitle = () => {
-        return this.title;
-    };
-    let setTitle = (title) => {
-        this.title = title;
-    };
-    let getYear = () => {
-        return this.year;
-    }
-    let setYear = (year) => {
-        this.year = year;
-    }
-    let getDirector = () => {
-        return this.director;
-    }
-    let setDirector = (director) => {
-        this.director = director;
-    }
-    
-    setId(id);
-    setTitle(title);
-    setYear(year);
-    setDirector(director);
+function Movie (id, title='Default', picture={}, year=2020, director='Default') {
+    this.id = id,
+    this.title = title,
+    this.picture = picture,
+    this.year = year,
+    this.director = director
 }
 
+const getMovie = (movie) => {
+    movie = movies.find(mov => mov.id === movie.id);
+    return movie;
+}
+
+const addMovie = (movie)=>{movies.push(movie)};
+
+const updateMovie = (movie) => {
+    const index = movies.findIndex(mov => mov.id === movie.id);
+    movies.splice(index, 1, movie);
+    return movie;
+}
+
+const deleteMovie = (movie) => {
+    movies = movies.filter(mov => mov.id !== movie.id);
+    return movies;
+}
 
 module.exports = {
     getMovies: ()=> {return movies},
-    addMovie: (Movie)=>{movies.push(Movie)},
+    getMovie: getMovie,
+    addMovie: addMovie,
+    updateMovie: updateMovie,
+    deleteMovie: deleteMovie,
     Movie: Movie
 }
